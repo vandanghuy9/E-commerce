14cbe582a35eb27182fb56817acd9993682265da
Vũ Trung Nghĩa <vutrungnghiahn@gmail.com>
2023-11-21 23:27:10 +0700

    merge from rouie

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,32 @@
-import React from "react";
-import "@/styles/globals.css";
-import { Layout } from "./components";
+/* eslint-disable react-hooks/exhaustive-deps */
 import { StateContext } from "@/context/StateContext";
+import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
+import { Layout } from "./components";
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const { pathname } = router;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    router.push("/signin");
+    setTimeout(() => {
+      setLoading(false);
+    }, 300);
+  }, []);
+
   return (
     <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
+      {!loading &&
+        (["/signin", "/signup"].includes(pathname) ? (
+          <Component {...pageProps} />
+        ) : (
+          <Layout>
+            <Toaster />
+            <Component {...pageProps} />
+          </Layout>
+        ))}
     </StateContext>
   );
 }

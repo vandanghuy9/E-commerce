06c2ba908b6789de28100b07fbb0d12edc462aaa
vandanghuy9 <vandanghuy9@gmail.com>
2023-12-18 12:49:35 +0700

    product detail

--- src/data/seeder.sql
+++ src/data/seeder.sql
@@ -1,4 +1,8 @@
-insert into app_productimage (image, link, type, product_id) values
+insert into app_supplier (name) values ('A company');
+
+insert into app_category (name) values ('headphones','shoes', 'hat','sock');
+
+insert into app_product (name, content, avarage_rating, price, category_id, supplier_id) values
 ("Earphones 1","New product",9,50.0,1,1),
 ("Earphones 2","New product",9,50.0,1,1),
 ("Earphones 3","New product",9,50.0,1,1),
@@ -32,3 +36,11 @@ insert into app_productimage (image, link, type, product_id) values
 ("product 6","/banner/assets/headphones_c_2.webp","image", 6),
 ("product 6","/banner/assets/headphones_c_3.webp","image", 6),
 ("product 6","/banner/assets/headphones_c_4.webp","image", 6);
+
+insert into app_review (content, rating, time, user_id, product_id)
+values 
+('Great app!', 5, NOW(), 1, 1), 
+('I really liked this app', 9, NOW(), 1, 2),
+('This app has a great user interface, but the lack of features makes it less useful than other apps in the same category', 5, NOW(), 1, 3),
+('I knocked it down one star because the app crashes frequently,', 5, NOW(), 1, 4),
+('Great app! shit', 5, NOW(), 1, 5);
---
+++ src/pages/components/ProductReview.js
@@ -0,0 +1,28 @@
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+const ProductReview = ({ rating, time, content, username }) => {
+  let stars = [];
+  let maxScore = 5;
+  for (let i = 0; i < rating / 2; i++) {
+    stars.push(1);
+  }
+  for (let i = rating / 2; i < maxScore; i++) {
+    stars.push(0);
+  }
+  return (
+    <div class="product-comment-main border-b border-black py-4">
+      <div class="product-comment-author">{username}</div>
+      <div class="repeat-purchase-con">
+        <div class="flex items-center">
+          {stars.map((item, i) =>
+            item === 1 ? <AiFillStar key={i} /> : <AiOutlineStar key={i} />
+          )}
+        </div>
+      </div>
+      <div class="product-rating-time my-2 text-sm text-gray-500">{time}</div>
+      <div class="relative my-2 font-sm leading-6 text-black w-full">
+        {content}
+      </div>
+    </div>
+  );
+};
+export default ProductReview;

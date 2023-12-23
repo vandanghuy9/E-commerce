0852896be840ccb62d598c2f48d16b1065afaf62
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-12-23 21:28:40 +0700

    fixbug/image

--- src/pages/order.js
+++ src/pages/order.js
@@ -19,7 +19,7 @@ const Order = () => {
   return (
     <div>
       <div className="pt-12 pb-2">
-        <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg md:max-w-5xl">
+        <div className="max-w-md mx-auto bg-gray-100 rounded-lg shadow-lg md:max-w-5xl">
           <div className="md:flex-col">
             <div className="w-full px-5 py-2">
               <div className="p-5">
@@ -27,41 +27,41 @@ const Order = () => {
                   <FiUser className="w-6 h-6" />
                 </div>
                 <h2 className="text-xl font-medium ">Address</h2>
-                <div className="py-5 items-center ">
-                  <div className="grid grid-cols-4 text-base gap-2 relative">
+                <div className="items-center py-5 ">
+                  <div className="relative grid grid-cols-4 gap-2 text-base">
                     <span className="font-bold">Name</span>
-                    <span className="mx-4 break-all col-span-2">Address</span>
+                    <span className="col-span-2 mx-4 break-all">Address</span>
                   </div>
                 </div>
               </div>
             </div>
             <div className="py-2">
-              <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg md:max-w-5xl">
+              <div className="max-w-md mx-auto bg-gray-100 rounded-lg shadow-lg md:max-w-5xl">
                 <div className="md:flex">
                   <div className="w-full px-5 py-2">
                     <div className="p-5">
-                      <div className="flex gap-2 justify-between text-red-500 items-center">
-                        <h2 className="text-xl font-medium pr-8">
+                      <div className="flex items-center justify-between gap-2 text-red-500">
+                        <h2 className="pr-8 text-xl font-medium">
                           Products Ordered
                         </h2>
-                        <div className="text-xl pr-8">Price</div>
-                        <div className="text-xl pr-8">Quantity</div>
-                        <div className="text-xl pr-8">Subtotal</div>
+                        <div className="pr-8 text-xl">Price</div>
+                        <div className="pr-8 text-xl">Quantity</div>
+                        <div className="pr-8 text-xl">Subtotal</div>
                       </div>
                       {cartItems.map((item) => (
                         <div
                           key={item.id}
-                          className="flex justify-between items-center mt-6 pt-6"
+                          className="flex items-center justify-between pt-6 mt-6"
                         >
                           <div className="flex items-center">
                             <Image
-                              src={item.images[0].link}
+                              src={item.images[0]?.link}
                               alt="Product"
                               width={20}
                               height={20}
                             />
                             <div className="flex flex-col ml-3">
-                              <span className="md:text-md font-medium">
+                              <span className="font-medium md:text-md">
                                 {item.name}
                               </span>
                             </div>
@@ -69,7 +69,7 @@ const Order = () => {
                           <div className="flex items-center">
                             {/* Price */}
                             <div className="pr-8">
-                              <span className="text-md font-medium">
+                              <span className="font-medium text-md">
                                 {item.price}
                               </span>
                             </div>
@@ -81,7 +81,7 @@ const Order = () => {
                           <div className="justify-center">
                             {/* Subtotal */}
                             <div className="pr-8">
-                              <span className="text-md font-medium text-red-500">
+                              <span className="font-medium text-red-500 text-md">
                                 {item.quantity * item.price}
                               </span>
                             </div>
@@ -89,9 +89,9 @@ const Order = () => {
                         </div>
                       ))}
 
-                      <div className="flex justify-between items-center mt-6 pt-6 border-t">
-                        <div className="flex justify-center items-end">
-                          <span className="text-lg font-medium text-gray-400 mr-1">
+                      <div className="flex items-center justify-between pt-6 mt-6 border-t">
+                        <div className="flex items-end justify-center">
+                          <span className="mr-1 text-lg font-medium text-gray-400">
                             Shipping fee: $
                           </span>
                           <span className="text-lg font-bold text-gray-800">
@@ -105,13 +105,13 @@ const Order = () => {
               </div>
             </div>
             <div className="py-2">
-              <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg md:max-w-5xl">
+              <div className="max-w-md mx-auto bg-gray-100 rounded-lg shadow-lg md:max-w-5xl">
                 <div className="md:flex">
                   <div className="w-full px-5 py-2">
                     <form action="/order" method="post">
                       <div className="grid grid-cols-4 p-5">
                         <div className="flex text-red-500">
-                          <h2 className="text-xl font-medium pr-8">
+                          <h2 className="pr-8 text-xl font-medium">
                             Payment method
                           </h2>
                         </div>
@@ -128,17 +128,17 @@ const Order = () => {
                       <div className="grid grid-cols-4 p-5">
                         <div className="col-span-3"></div>
                         <div>
-                          <div className="text-lg font-medium mr-1">
+                          <div className="mr-1 text-lg font-medium">
                             <span className="text-gray-400">Bill total: </span>
                             <span className="font-bold">${totalPrice}</span>
                           </div>
-                          <div className="text-lg font-medium mr-1">
+                          <div className="mr-1 text-lg font-medium">
                             <span className="text-gray-400">
                               Shipping Fee:{" "}
                             </span>
                             <span className="font-bold">${shippingFee}</span>
                           </div>
-                          <div className="text-lg font-medium mr-1">
+                          <div className="mr-1 text-lg font-medium">
                             <span className="text-gray-400">
                               Total payment:
                             </span>

13775a9005472ec8fbea3992e9a1714d84b0d534
Vũ Trung Nghĩa <vutrungnghiahn@gmail.com>
2023-12-24 16:42:34 +0700

    openai integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Layout } from "./components";
 import UserContext from "@/context/UserContext";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
   // const router = useRouter();
   // const { pathname } = router;
@@ -18,14 +19,20 @@ export default function App({ Component, pageProps }) {
   // }, []);
 
   return (
-    <UserContext>
-      <StateContext>
-        <Layout>
-          <Toaster />
-          <Component {...pageProps} />
-        </Layout>
-      </StateContext>
-    </UserContext>
+    <>
+      <Script
+        src="https://www.socialintents.com/api/socialintents.1.3.js#2c9faa358c852b2a018c9b1a4bcb0b2b"
+        async="async"
+      />
+      <UserContext>
+        <StateContext>
+          <Layout>
+            <Toaster />
+            <Component {...pageProps} />
+          </Layout>
+        </StateContext>
+      </UserContext>
+    </>
   );
 }
 
